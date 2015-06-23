06a8c197a5a7e8293d1b63d41d2f557967ae579d
visavi <visavi.net@mail.ru>
2015-06-23 12:01:21 +0300

    bugfix checkcompress method

--- src/Compressor.php
+++ src/Compressor.php
@@ -90,7 +90,7 @@ class Compressor {
 		if (strpos($encoding, 'gzip') !== false)
 			$support = 'gzip';
 		elseif(strpos($encoding, 'x-gzip') !== false)
-        	$encoding = 'x-gzip';
+			$support = 'x-gzip';
 		elseif (strpos($encoding, 'deflate') !== false)
 			$support = 'deflate';
 		else
